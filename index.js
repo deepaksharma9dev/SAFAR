61ddde143681545fa448e2a4c18d8a8a9df199dc
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-08-02 21:58:35 +0530

    maneged db.js

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use((err, req, res, next) => {
 
 });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
