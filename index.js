221d9ce94ab3afb5e1a776f034fcfc37d68f2f7a
Deepak-Sharma28 <dk1999sharma1999@gmail.com>
2021-08-02 22:54:11 +0530

    changed the db files

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const express = require('express');
 const app = express();
-const db = require('./Config/db');
+const db = require('./config/db');
 
 //routes
 const Admin = require('./routers/Admin');
@@ -28,7 +28,7 @@ app.use('/api/user/auth', auth);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname.anchor, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
 }
 
